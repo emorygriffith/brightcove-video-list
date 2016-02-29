71b6bd094e964b04aca9ce03e79e67dab250db30
Emory Griffith <emorygriffith91@gmail.com>
2016-02-28 23:08:40 -0500

    adding vids now

---
+++ app/scripts/data.js
@@ -0,0 +1,31 @@
+var data = [
+      {
+          "title": "Emory Sample Video",
+          "description": "Short video I created",
+          "id": "1",
+          "length": "15",
+          "author ": "Emory",
+          "src": "http://players.brightcove.net/4744530050001/default_default/index.html?videoId=4766574034001"
+      },
+      {
+          "title": "Sample Video 1",
+          "description": "This short description can be used to give more info...",
+          "id": "2",
+          "length": "8",
+          "author ": "Brightcove",
+          "src": "http://players.brightcove.net/4744530050001/default_default/index.html?videoId=4744690338001"
+      },
+      {
+          "title": "Sample Video 1",
+          "description": "This short description can be used to give more info...",
+          "id": "2",
+          "length": "29",
+          "author ": "Brightcove",
+          "src": "http://players.brightcove.net/4744530050001/default_default/index.html?videoId=4744690337001"
+      }
+    ];
+
+
+var template = _.template($('#video-list').html());
+var html = template(data);
+$(".vids").html(html);
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,43 +1,45 @@
-var account_username = 'emorygriffith91@gmail.com';
-var account_password = 'Ahem210fire765!';
-var account_id = '4744530050001';
-var call_url = 'https://players.api.brightcove.com/v1/accounts/' + account_id + '/players';
-
-var makeAjaxCall = function (callURL, callType, callData) {
-  if (callData) {
-    $.ajax({
-      type: callType,
-      headers: {
-        "Authorization": "Basic " + btoa(account_username + ":" + account_password),
-        "Content-Type": "application/json"
-      },
-      url: callURL,
-      data: JSON.stringify(callData),
-      success: ajaxSuccess,
-    //  error: ajaxError
-    });
-  } else {
-    $.ajax({
-      type: callType,
-      headers: {
-        "Authorization": "Basic " + btoa(account_username + ":" + account_password),
-        "Content-Type": "application/json"
-      },
-      url: callURL,
-      success: ajaxSuccess,
-    //  error: ajaxError
-    });
-  }
-};
-
-var ajaxSuccess = function (data) {
-	var results = data.items;
-	$.each(results, function(key, value){
-
-		console.log(value);
-		// document.getElementById("jsonResponse").innerHTML = JSON.stringify(data);
-	})
-
-};
-
-makeAjaxCall(call_url, "GET", null);
+// var account_username = 'emorygriffith91@gmail.com';
+// var account_password = 'passwordForMe1';
+// var account_id = '4744530050001';
+// var call_url = 'https://players.api.brightcove.com/v1/accounts/' + account_id + '/players';
+//
+// var makeAjaxCall = function (callURL, callType, callData) {
+//   if (callData) {
+//     $.ajax({
+//       type: callType,
+//       headers: {
+//         "Authorization": "Basic " + btoa(account_username + ":" + account_password),
+//         "Content-Type": "application/json"
+//       },
+//       url: callURL,
+//       data: JSON.stringify(callData),
+//       success: ajaxSuccess,
+//     //  error: ajaxError
+//     });
+//   } else {
+//     $.ajax({
+//       type: callType,
+//       headers: {
+//         "Authorization": "Basic " + btoa(account_username + ":" + account_password),
+//         "Content-Type": "application/json"
+//       },
+//       url: callURL,
+//       success: ajaxSuccess,
+//       //error: ajaxError
+//     });
+//   }
+// };
+//
+// var ajaxSuccess = function (data) {
+//
+// 	var results = data.items;
+// 	console.log(results);
+//
+// 	$.each(results, function(key, value){
+//
+// //		 $('.jsonResponse').append("<div>" + value.name + "  " + value.id + "</div>")
+// 	});
+//
+// };
+//
+// makeAjaxCall(call_url, "GET", null);
